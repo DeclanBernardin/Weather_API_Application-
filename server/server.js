926489addcc64094187654f6545ab9c2e72452e2
Declan Bernardin <dmjbernardin@yahoo.com>
2020-02-12 20:31:57 -0600

    set up server to recieve and direct axios input towards search route

--- server/server.js
+++ server/server.js
@@ -3,3 +3,21 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');  
 
+// Routes Origin 
+const searchRouter = require('./routes/search.router')
+
+// Body parser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Serve static files
+app.use(express.static('build'));
+
+// Routes
+app.use('/api/search', searchRouter)
+
+const PORT = process.env.PORT || 5000; 
+
+app.listen(PORT, () => {
+    console.log(`on port: ${PORT}`)
+});
